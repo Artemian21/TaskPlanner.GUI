6747f137c9a4e2876745f7da8dcd07a5e2ca0506
Artem Yaremenko <artemyaremenko21@gmail.com>
2025-03-10 20:46:27 +0200

    Replaced the standard dependency injection for ASP.NET with Autofac and wrote unit tests for the business logic level

---
+++ TaskPlanner.Tests/ProjectServiceTests.cs
@@ -0,0 +1,120 @@
+﻿using AutoFixture;
+using Moq;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskPlanner.BusinessLogic.Services;
+using TaskPlanner.Domain.Abstraction;
+using TaskPlanner.Domain.Models;
+
+namespace TaskPlanner.Tests
+{
+    public class ProjectServiceTests
+    {
+        private readonly IUnitOfWork _unitOfWorkMock;
+        private readonly IProjectService _projectService;
+        private readonly IProjectRepository _projectRepositoryMock;
+        private readonly Fixture _fixture;
+
+        public ProjectServiceTests()
+        {
+            _unitOfWorkMock = Substitute.For<IUnitOfWork>();
+            _projectRepositoryMock = Substitute.For<IProjectRepository>();
+            _unitOfWorkMock.ProjectRepository.Returns(_projectRepositoryMock);
+            _projectService = new ProjectService(_unitOfWorkMock);
+            _fixture = new Fixture();
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task GetAllProjects_ShouldReturnAllProjects()
+        {
+            var projects = _fixture.CreateMany<Project>().ToList();
+            _projectRepositoryMock.GetAllAsync().Returns(projects);
+
+            var result = await _projectService.GetAllProjects();
+
+            Assert.NotNull(result);
+            Assert.Equal(projects, result);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task GetProjectById_ShouldReturnProject()
+        {
+            var project = _fixture.Create<Project>();
+            _projectRepositoryMock.GetByIdAsync(Arg.Any<Guid>()).Returns(project);
+
+            var result = await _projectService.GetProjectById(project.Id);
+
+            Assert.NotNull(result);
+            Assert.Equal(project, result);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task GetProjectById_ShouldReturnNull_WhenProjectNotFound()
+        {
+            _projectRepositoryMock.GetByIdAsync(Arg.Any<Guid>()).Returns((Project)null);
+            var result = await _projectService.GetProjectById(Guid.NewGuid());
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task AddProject_ShouldReturnProject()
+        {
+            var project = _fixture.Create<Project>();
+            _projectRepositoryMock.AddAsync(Arg.Any<Project>()).Returns(project);
+
+            var result = await _projectService.AddProject(project);
+
+            Assert.NotNull(result);
+            Assert.Equal(project, result);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task DeleteProject_ShouldReturnTrue()
+        {
+            var projectId = Guid.NewGuid();
+            _projectRepositoryMock.DeleteAsync(Arg.Any<Guid>()).Returns(true);
+
+            var result = await _projectService.DeleteProject(projectId);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task DeleteProject_ShouldReturnFalse_WhenProjectNotFound()
+        {
+            var projectId = Guid.NewGuid();
+            _projectRepositoryMock.DeleteAsync(Arg.Any<Guid>()).Returns(false);
+
+            var result = await _projectService.DeleteProject(projectId);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task UpdateProject_ShouldReturnProject()
+        {
+            var project = _fixture.Create<Project>();
+            _projectRepositoryMock.UpdateAsync(Arg.Any<Guid>(), Arg.Any<string>(), Arg.Any<string>(), Arg.Any<DateTime?>()).Returns(project);
+
+            var result = await _projectService.UpdateProject(project.Id, project.Name, project.Description, project.Deadline);
+
+            Assert.NotNull(result);
+            Assert.Equal(project, result);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task UpdateProject_ShouldReturnNull_WhenProjectNotFound()
+        {
+            _projectRepositoryMock.UpdateAsync(Arg.Any<Guid>(), Arg.Any<string>(), Arg.Any<string>(), Arg.Any<DateTime?>()).Returns((Project)null);
+
+            var result = await _projectService.UpdateProject(Guid.NewGuid(), "name", "description", DateTime.Now);
+
+            Assert.Null(result);
+        }
+    }
+}
---
+++ TaskPlanner.Tests/TaskServiceTests.cs
@@ -0,0 +1,138 @@
+﻿using AutoFixture;
+using Moq;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskPlanner.BusinessLogic.Services;
+using TaskPlanner.Domain.Abstraction;
+using TaskPlanner.Domain.Enums;
+using TaskPlanner.Domain.Models;
+
+namespace TaskPlanner.Tests
+{
+    public class TaskServiceTests
+    {
+        private readonly IUnitOfWork _unitOfWorkMock;
+        private readonly ITaskService _taskService;
+        private readonly ITaskRepository _taskRepositoryMock;
+        private readonly Fixture _fixture;
+
+        public TaskServiceTests()
+        {
+            _unitOfWorkMock = Substitute.For<IUnitOfWork>();
+            _taskRepositoryMock = Substitute.For<ITaskRepository>();
+            _unitOfWorkMock.TaskRepository.Returns(_taskRepositoryMock);
+            _taskService = new TaskService(_unitOfWorkMock);
+            _fixture = new Fixture();
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task GetAllTasks_ShouldReturnAllTasks()
+        {
+            var tasks = _fixture.CreateMany<TaskPlanner.Domain.Models.Task>().ToList();
+            _taskRepositoryMock.GetAllAsync().Returns(tasks);
+
+            var result = await _taskService.GetAllTasks();
+
+            Assert.NotNull(result);
+            Assert.Equal(tasks, result);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task GetTaskById_ShouldReturnTask()
+        {
+            var task = _fixture.Create<TaskPlanner.Domain.Models.Task>();
+            _taskRepositoryMock.GetByIdAsync(Arg.Any<Guid>()).Returns(task);
+
+            var result = await _taskService.GetTaskById(task.Id);
+
+            Assert.NotNull(result);
+            Assert.Equal(task, result);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task GetTaskById_ShouldReturnNull_WhenTaskNotFound()
+        {
+            var task = _fixture.Create<TaskPlanner.Domain.Models.Task>();
+            _taskRepositoryMock.GetByIdAsync(Arg.Any<Guid>()).Returns((TaskPlanner.Domain.Models.Task)null);
+
+            var result = await _taskService.GetTaskById(task.Id);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task AddTask_ShouldReturnTask()
+        {
+            var task = _fixture.Create<TaskPlanner.Domain.Models.Task>();
+            var project = _fixture.Build<Project>()
+                          .With(p => p.Id, task.ProjectId)
+                          .Create();
+            _unitOfWorkMock.ProjectRepository.GetByIdAsync(task.ProjectId).Returns(project);
+            _unitOfWorkMock.TaskRepository.AddAsync(Arg.Any<TaskPlanner.Domain.Models.Task>()).Returns(task);
+
+            var result = await _taskService.AddTask(task);
+
+            Assert.NotNull(result);
+            Assert.Equal(task, result);
+        }
+
+
+        [Fact]
+        public async System.Threading.Tasks.Task AddTask_ShouldThrowException_WhenProjectNotFound()
+        {
+            var task = _fixture.Create<TaskPlanner.Domain.Models.Task>();
+            _unitOfWorkMock.ProjectRepository.GetByIdAsync(task.ProjectId).Returns((Project)null);
+
+            await Assert.ThrowsAsync<ArgumentException>(() => _taskService.AddTask(task));
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task DeleteTask_ShouldReturnTrue()
+        {
+            var task = _fixture.Create<TaskPlanner.Domain.Models.Task>();
+            _taskRepositoryMock.DeleteAsync(Arg.Any<Guid>()).Returns(true);
+
+            var result = await _taskService.DeleteTask(task.Id);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task DeleteTask_ShouldReturnFalse_WhenTaskNotFound()
+        {
+            var task = _fixture.Create<TaskPlanner.Domain.Models.Task>();
+            _taskRepositoryMock.DeleteAsync(Arg.Any<Guid>()).Returns(false);
+
+            var result = await _taskService.DeleteTask(task.Id);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task UpdateTask_ShouldReturnTask()
+        {
+            var task = _fixture.Create<TaskPlanner.Domain.Models.Task>();
+            _taskRepositoryMock.UpdateAsync(Arg.Any<Guid>(), Arg.Any<string>(), Arg.Any<string>(), Arg.Any<DateTime?>(), Arg.Any<Domain.Enums.TaskStatus>(), Arg.Any<PriorityStatus>()).Returns(task);
+
+            var result = await _taskService.UpdateTask(task.Id, task.Title, task.Description, task.Deadline, task.Status, task.Priority);
+
+            Assert.NotNull(result);
+            Assert.Equal(task, result);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task UpdateTask_ShouldReturnNull_WhenTaskNotFound()
+        {
+            var task = _fixture.Create<TaskPlanner.Domain.Models.Task>();
+            _taskRepositoryMock.UpdateAsync(Arg.Any<Guid>(), Arg.Any<string>(), Arg.Any<string>(), Arg.Any<DateTime?>(), Arg.Any<Domain.Enums.TaskStatus>(), Arg.Any<PriorityStatus>()).Returns((TaskPlanner.Domain.Models.Task)null);
+
+            var result = await _taskService.UpdateTask(task.Id, task.Title, task.Description, task.Deadline, task.Status, task.Priority);
+
+            Assert.Null(result);
+        }
+    }
+}
